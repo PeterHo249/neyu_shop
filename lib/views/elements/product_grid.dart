017fbd52f541855fd85d1d2e4407a3a905861129
PeterHo249 <peterho249@outlook.com>
2021-03-30 22:02:21 +0700

    product init

--- lib/views/elements/product_grid.dart
+++ lib/views/elements/product_grid.dart
@@ -1,15 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:neyu_shop/models/product.dart';
 
 class ProductGrid extends StatelessWidget {
+  final productList = [
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 1',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 2',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 3',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 4',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 5',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 6',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 7',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 8',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 9',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 10',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 11',
+      500000,
+    ),
+    Product(
+      '1',
+      'assets/img/placeholder.png',
+      'product 12',
+      500000,
+    ),
+  ];
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue.shade100,
-      width: 500,
-      height: 500,
-      child: Text(
-        'This is product list',
+    return GridView.builder(
+      itemCount: productList.length,
+      physics: NeverScrollableScrollPhysics(),
+      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 5,
+        mainAxisExtent: 15.0,
+        crossAxisSpacing: 15.0,
       ),
+      itemBuilder: (BuildContext context, int index) {
+        return Container(
+          height: 50,
+          width: 50,
+        );
+      },
     );
   }
 }
